6dbe85a8b4456b39e5def0fab7ae85a111f0f036
SubukaSutulai <71166299+SubukaSutulai@users.noreply.github.com>
2022-02-14 16:20:59 +0400

    Home Work 3d ay

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -5,4 +5,26 @@ const swiper = new Swiper('.main-slider', {
     navigation: {
       nextEl: '.main_slider-arrow'
     },
+    breakpoints:
+    {
+      320:
+      {
+        slidesPerView: 1,
+      },
+      768:
+      {
+        slidesPerView: 2,
+      }
+    }
   });
+  let burger = document.querySelector('.menu-burger');
+  let closeMenu = document.querySelector('.header_menu-close');
+  let menuModile = document.querySelector('.header');
+  burger.addEventListener('click', ()=>
+  {
+    menuModile.style.display = 'block';
+  })
+  closeMenu.addEventListener('click', ()=>
+  {
+    menuModile.style.display = 'none';
+  })
